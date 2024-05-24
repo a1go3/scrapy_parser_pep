432a04e380a44aca1503d6054a7423aaab31434d
Roman <flexomj@gmail.com>
2024-05-24 18:34:43 +0300

    update

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -20,8 +20,10 @@ class PepParsePipeline:
 
     def process_item(self, item, spider):
         self.total = len(item['status'])
-
-        self.results[item['status']] += 1
+        if item['status'] is not None:
+            self.results[item['status']] += 1
+        else:
+            self.results['Draft'] += 1
         return item
 
     def close_spider(self, spider):
@@ -35,3 +37,26 @@ class PepParsePipeline:
                  *(self.results.items()),
                  ['Total', sum(self.results.values())]
                  ])
+
+
+
+# class PepParsePipeline:
+#
+#     def open_spider(self, spider):
+#         self.statuses = defaultdict(lambda: 0)
+#
+#     def process_item(self, item, spider):
+#         status = item['status']
+#         self.statuses[status] += 1
+#         return item
+#
+#     def close_spider(self, spider):
+#         results = [('Статус', 'Количество')]
+#         results.extend(self.statuses.items())
+#         total = sum(self.statuses.values())
+#         results.append(('Total', total))
+#         time = dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+#         file_path = BASE_DIR / f'results/status_summary_{time}.csv'
+#         with open(file_path, 'w', encoding='utf-8') as file:
+#             csv_writer = csv.writer(file, dialect='unix')
+#             csv_writer.writerows(results)
