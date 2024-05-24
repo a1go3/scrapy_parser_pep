30b72b0eb3f7f2da0b0db7accb839b8ef3d2e1ef
Roman <flexomj@gmail.com>
2024-05-24 18:15:06 +0300

    update

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,33 +1,37 @@
-from collections import defaultdict
-from itemadapter import ItemAdapter
 from pathlib import Path
-
+import csv
+from collections import defaultdict
+from datetime import datetime
 from .utils import get_result_pep, file_output
 
-BASE_DIR = Path(__file__).parent
-
+BASE_DIR = Path(__file__).parent.parent
 DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+FILENAME = 'status_summary_{time}.csv'
+
 
 
 class PepParsePipeline:
-    counts = defaultdict(int)
+    def __init__(self):
+        self.results_dir = BASE_DIR / 'results'
+        self.results_dir.mkdir(exist_ok=True)
 
     def open_spider(self, spider):
-        ...
+        self.results = defaultdict(int)
 
     def process_item(self, item, spider):
+        self.total = len(item['status'])
 
-        # counts = defaultdict(int)
-
-        # adapter = ItemAdapter(item)
-        status = item['status']
-        # status = adapter.get('status')
-        self.counts[status] += 1
-        result = get_result_pep(self.counts)
-        file_output(result)
+        self.results[item['status']] += 1
         return item
 
     def close_spider(self, spider):
-        # result = get_result_pep(self.counts)
-        # return file_output(result)
-        pass
+        file_path = self.results_dir / FILENAME.format(
+            time=datetime.now().strftime(DATETIME_FORMAT))
+        with open(file_path, mode='w',
+                  encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile, dialect='unix')
+            writer.writerows(
+                [['Статус', 'Количество'],
+                 *(self.results.items()),
+                 ['Total', sum(self.results.values())]
+                 ])
